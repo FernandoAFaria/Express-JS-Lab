044e48344f390123ee8cc313d90c5e3190f31a83
Fernando Faria <fernandoafaria@yahoo.com>
2018-11-09 10:00:45 -0500

    finished advanced, will now add to JSON file, and display on /formsubmissions

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 
+
+//Body parser is used to grab data from input forms
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 //This will only return a single file//
@@ -18,20 +21,42 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.post('/submit', (req, res, next) => {
+
+    //This will grab the JSON file and load it into a variable.  Then it will push the input from the submit form and write out the file to disk
+    let data = fs.readFileSync(path.join(__dirname, '../data.json'))
+    let json = JSON.parse(data);
+    console.log(data)
+
     let email = req.body.email;
     let pw = req.body.password;
-    let obj1 = JSON.stringify({
+    let obj1 = {
         Email: email,
         Password: pw
-    })
-    fs.appendFileSync('data.json', obj1)
-    res.send(`Account registered for ${email}.  Thank you!`)
+    }
+    json.users.push(obj1)
+
+    fs.writeFile(path.join(__dirname, '../data.json'), JSON.stringify(json), 'utf-8')
+
+    res.send('Form Submitted!')
     
 })
 
+app.use('/formsubmissions', (req, res) => {
+
+    let data = fs.readFileSync(path.join(__dirname, '../data.json'))
+    let json = JSON.parse(data);
+    console.log(json)
+    res.send(json)
+    
+
+})
+    
+
+
+//THis makes all files in the public folder available for the browser
 app.use(express.static(path.join(__dirname, '../public')));
 
 
 
 
-app.listen(3000)
+app.listen(3000);
